3b0cae58046d12a7dcca46beee9e4e371011d89d
afrizalsofyan <afrizalafaandy@gmail.com>
2022-06-21 04:34:12 -0700

    fix: fix code fetchData feature

--- fetchData.js
+++ fetchData.js
@@ -4,15 +4,14 @@ const dataUser = fetch('https://jsonplaceholder.typicode.com/users')
     return response.json()
 })
 .then((datas)=>{
-    let data = datas
     let arrName = []
-    data.map((el)=>{
+    datas.map((el)=>{
         let names = `${el.name}`
         // console.log(name);
         return arrName.push(names)
     })
     console.log(arrName.join(" - "))
 })
-.catch(()=>console.log("Error!!! Url Invalid."))
+.catch((e)=>console.log(e.message))
 
 dataUser
